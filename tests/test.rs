75d4505795bde9a306c1f34d334a5675f8d55b8f
theorangepotato <github@alphahotelbravo.io>
2019-09-08 10:50:43 -0700

    Implement encode() and encode_len()
    - Added a test each for encode() and encode_len();

--- tests/test.rs
+++ tests/test.rs
@@ -6,11 +6,28 @@ fn should_encode_decode() {
   bits[8] = 0b00000001;
 
   let enc = bitfield_rle::encode(&bits);
-  assert_eq!(enc.len(), 6);
+  assert_eq!(enc.len(), 4);
 
   let res = bitfield_rle::decode(enc).unwrap();
 
   assert_eq!(res[8], 0b00000001);
+  assert_eq!(res, bits);
+}
+
+#[test]
+fn encode_len() {
+  let bitfield = vec![255, 255, 85, 84, 0, 0, 0, 183];
+  let len = bitfield_rle::encode_len(bitfield);
+  assert_eq!(len, 7);
+}
+
+#[test]
+fn encode() {
+  let bitfield = vec![255, 255, 85, 84, 0, 0, 0, 183];
+  let enc = bitfield_rle::encode(&bitfield);
+  let correct = vec![11, 4, 85, 84, 13, 2, 183];
+  assert_eq!(enc.len(), correct.len());
+  assert_eq!(enc, correct);
 }
 
 #[test]
