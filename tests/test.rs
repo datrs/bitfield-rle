96e3a39e74cd3405d1a8ed2339887c0be45b5562
Bruno Tavares <connect+github@bltavares.com>
2020-02-07 19:37:50 -0300

    Bump to 2018 edition and add clippy on travis
    This commit bumps the code to use the 2018 edition.
    
    It also adds more tests based on the node js suite, with fixes to make
    it run on Travis.
    
    The implementation between NodeJS and Rust differs on payloadsize with
    many extra bits on the end, but they are interoperable.

--- tests/test.rs
+++ tests/test.rs
@@ -1,5 +1,3 @@
-extern crate bitfield_rle;
-
 #[test]
 fn should_encode_decode() {
   let mut bits: Vec<u8> = vec![0; 16];
@@ -42,4 +40,48 @@ fn decode() {
   let res = bitfield_rle::decode(enc).unwrap();
   let correct = vec![255, 255, 85, 84, 0, 0, 0, 183];
   assert_eq!(res, correct);
+}
+
+#[test]
+fn not_power_of_two() {
+  let deflated = bitfield_rle::encode(vec![255, 255, 255, 240]);
+  let inflated = bitfield_rle::decode(deflated).unwrap();
+  assert_eq!(inflated, vec![255, 255, 255, 240]);
+}
+
+#[test]
+/// Differs on NodeJS: node trims final bits when 0 and returns a smaller payload
+/// Decoding returns the same result, but encoding the result is smaller
+/// Both are interoperable, with the different on the payload size when reading from node.
+///
+/// ```js
+/// require('bitfield-rle').encode(Buffer.from([])) // => <Buffer >
+/// require('bitfield-rle').decode(Buffer.from([])) // => <Buffer >
+/// require('bitfield-rle').decode(Buffer.from([0])) // => <Buffer >
+/// ```
+fn encodes_empty_bitfield() {
+  assert_eq!(
+    bitfield_rle::decode(bitfield_rle::encode(vec![])).unwrap(),
+    vec![]
+  );
+  assert_eq!(bitfield_rle::decode(vec![]).unwrap(), vec![]);
+  assert_eq!(bitfield_rle::decode(vec![0]).unwrap(), vec![]);
+  assert_eq!(bitfield_rle::encode(vec![]), vec![0]);
+}
+
+#[test]
+/// Differs on NodeJS: node trims final bits when 0 and returns a smaller payload
+/// Decoding returns the same result, but encoding the result is smaller.
+/// Both are interoperable, with the different on the payload size when reading from node.
+///
+/// ```js
+/// var data = require('bitfield-rle').decode(Buffer.from([2, 64, 253, 31])) // => <Buffer 40 00...>
+/// var data = require('bitfield-rle').encode(data) // => <Buffer 02 40>
+/// var data = require('bitfield-rle').encode(data) // => <Buffer 40> skipping the last bits
+/// ```
+fn does_not_trims_remaining_bytes() {
+  let mut bitfield = vec![0; 1024];
+  bitfield[0] = 64;
+  assert_eq!(bitfield_rle::encode_len(&bitfield), 4);
+  assert_eq!(bitfield_rle::encode(&bitfield), vec![2, 64, 253, 31]);
 }
